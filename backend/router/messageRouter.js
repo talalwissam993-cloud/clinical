1f4560ad6179dd6cb29db427b34e8e304b84a013
talalwissam993-cloud <talalwissam993@gmail.com>
2026-01-21 07:03:16 +0300

    Update messageRouter.js

--- backend/router/messageRouter.js
+++ backend/router/messageRouter.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
   getAllMessages,
   sendMessage,
+  deleteMessage,
 } from "../controller/messageController.js";
 import { isAdminAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/send", sendMessage);
 router.get("/getall", isAdminAuthenticated, getAllMessages);
-
+router.delete("/delete/:id", deleteMessage); // The type is DELETE
 export default router;
