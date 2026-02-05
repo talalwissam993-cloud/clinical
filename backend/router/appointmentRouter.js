e62b0086b423ea1ebb996bdbf1917beaa8df467b
talalwissam993-cloud <talalwissam993@gmail.com>
2026-02-05 23:49:18 +0300

    Update appointmentRouter.js

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -18,6 +18,5 @@ router.get("/getall", isAdminAuthenticated, getAllAppointments);
 router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
 router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
 router.get("/getcount", isAdminAuthenticated, getAppointmentStats)
-router.get("/my", isPatientAuthenticated, getMyAppointments);
-
+router.get("/my-appointments", isPatientAuthenticated, getMyPatientAppointments);
 export default router;
