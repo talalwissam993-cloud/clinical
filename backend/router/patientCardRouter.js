7c93635c9e5cc0f15009e16266e53318221e6fc9
talalwissam993-cloud <talalwissam993@gmail.com>
2026-02-07 06:07:18 +0300

    Update patientCardRouter.js

--- backend/router/patientCardRouter.js
+++ backend/router/patientCardRouter.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get("/get/:patientId", controller.getPatientCard);
 
 // --- ADMIN ONLY ROUTES ---
-router.post("/upsert", isAdminAuthenticated, controller.upsertPatientCard);
+router.post("/upsert", controller.upsertPatientCard);
 router.get("/search/:query", controller.searchCardByNameOrNIC);
 router.delete("/delete-full/:id", isAdminAuthenticated, controller.deleteFullCard);
 
@@ -24,3 +24,4 @@ router.patch("/med/status/:cardId/:medId", isAdminAuthenticated, controller.upda
 
 router.get("/my-reminders", isPatientAuthenticated, controller.getActiveReminders);
 export default router;
+
