7e4ba6ca59030b01687e6a56a5a2c92861c091c6
talalwissam993-cloud <talalwissam993@gmail.com>
2026-02-07 06:21:15 +0300

    Update patientCardRouter.js

--- backend/router/patientCardRouter.js
+++ backend/router/patientCardRouter.js
@@ -8,8 +8,9 @@ const router = express.Router();
 // Use a logical OR check or a combined middleware if your auth supports it.
 // Here we allow both to access this specific endpoint.
 router.get("/get/:patientId", controller.getPatientCard);
+
 // --- ADMIN ONLY ROUTES ---
-router.post("/upsert",isAdminAuthenticated, controller.upsertPatientCard);
+router.post("/upsert", isAdminAuthenticated, controller.upsertPatientCard);
 router.get("/search/:query", controller.searchCardByNameOrNIC);
 router.delete("/delete-full/:id", isAdminAuthenticated, controller.deleteFullCard);
 
@@ -23,7 +24,3 @@ router.patch("/med/status/:cardId/:medId", isAdminAuthenticated, controller.upda
 
 router.get("/my-reminders", isPatientAuthenticated, controller.getActiveReminders);
 export default router;
-
-
-
-
