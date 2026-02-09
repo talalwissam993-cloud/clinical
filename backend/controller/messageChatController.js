50291d4fa86bcc03b7ac4448e9293aeeb914ebfa
talalwissam993-cloud <talalwissam993@gmail.com>
2026-02-09 03:29:23 +0300

    Update messageChatController.js

--- backend/controller/messageChatController.js
+++ backend/controller/messageChatController.js
@@ -1,14 +1,18 @@
-import { Message } from "../models/messageChatSchema.js";
+import { ChatMessage } from "../models/messageChatSchema.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 
-export const getAllMessages = catchAsyncErrors(async (req, res, next) => {
-    const messages = await Message.find().sort({ createdAt: 1 });
-    res.status(200).json({
-        success: true,
-        messages,
-    });
-});
+export const getAllMessages = async (req, res, next) => {
+    try {
+        const messages = await ChatMessage.find().sort({ createdAt: 1 });
+        res.status(200).json({
+            success: true,
+            messages,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
 // Note: Post messages are usually handled via Socket for real-time, 
 // but we keep a REST method as a backup.
@@ -17,10 +21,11 @@ export const sendMessage = catchAsyncErrors(async (req, res, next) => {
     if (!text) {
         return next(new ErrorHandler("Message text is required", 400));
     }
-    const message = await Message.create({
+    const messages = await ChatMessage.create({
         text,
         senderName,
         sender: req.user._id,
     });
     res.status(201).json({ success: true, message });
+
 });
