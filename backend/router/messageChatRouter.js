b7411ef5b175d46dd2688b879723e767fe134808
talalwissam993-cloud <talalwissam993@gmail.com>
2026-02-13 21:14:39 +0300

    Update messageChatRouter.js

--- backend/router/messageChatRouter.js
+++ backend/router/messageChatRouter.js
@@ -1,10 +1,22 @@
 import express from "express";
 import { getAllMessages, sendMessage } from "../controller/messageChatController.js";
-import { isPatientAuthenticated } from "../middlewares/auth.js";
+// Import BOTH authentication middlewares
+import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/all", isPatientAuthenticated, getAllMessages);
-router.post("/send", isPatientAuthenticated, sendMessage);
+/**
+ * We use a "custom" check or allow both. 
+ * If your 'isAdminAuthenticated' covers Doctors/Chemists/Nurses, 
+ * use it alongside isPatientAuthenticated.
+ */
+
+// 1. For fetching messages: Allow both Staff and Patients
+router.get("/all", getAllMessages); 
+
+// 2. For sending messages: 
+// If the user is a Doctor/Chemist, they pass 'isAdminAuthenticated'
+// If the user is a Patient, they pass 'isPatientAuthenticated'
+router.post("/send", isAdminAuthenticated, sendMessage);
 
 export default router;
